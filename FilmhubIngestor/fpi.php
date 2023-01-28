370d585659fa557be1e1737418b8c3a22b4510d3
Scott Cliburn <scott.cliburn@gmail.com>
2023-01-28 11:11:29 -0800

    README: FH<>MRSS

--- FilmhubIngestor/fpi.php
+++ FilmhubIngestor/fpi.php
@@ -1,42 +1,5 @@
 <?php
 ini_set('display_errors', 1);
-# no funny business yet, no arg passed, just run it. php fpi.php
-# s3://zype-filmhub
-
-/* FILMHUB (FH) PACKAGE INGESTOR (FPI)
- * The following script will interate over a Filhub Delivery Package directory and parse each YAML into an MRSS item for ingestion.
- * This script is available as is.
- * Author: Scott Cliburn
- * 
- * Detail Flow:
- * Step 1: Read in a directory list of the aws bucket, saved as a flat file to: dirFile in DIRLIST
- * Step 2: Iterate over the directory list and identify YAML files.
- * Step 3: Extract/Download YAML file and store locally as YAML and JSON, build objects file with yaml ids/file paths
- * Step 4: Read in Object file and iterate over it, building XML items (movies/episodes, trailers, series)
- *      Detailed mapping data in external document.
- *      Fields mapped into <item> elements stored as JSON for final iteration
- * Step 5: Read in each directory of items (movies/episodes, trailers, series). 
- *      Iterate over each and convert from json into xml and store inside genre folder (Filmhub genres used as primary category).
- *      Do this with trailers and series objects (parent to episodes) and build as individual items for mapping post ingest
- * Step 6: Read in each genre directory and iterate over each file (represents a single video)
- *      Build MRSS Import file compliant to zype.com
-
-    "DIRLIST"           => "dirlist/", keep untouched
-
-    "YMLJSON"           => "yaml_json/", // clean on full run
-    "YAML"              => "yaml/", // clean on full run
-    "XMLITEMS"          => "xmlitems/", // clean on full run
-    "XMLSERIESITEMS"    => "xmlitems_series/", // clean on full run
-    "XMLTRAILERITEMS"   => "xmlitems_trailers/", // clean on full run
-    "OBJECTS"           => "objects/", // clean on full run
-    "SKUIDS"            => "skuids/", // clean on full run
-    "BADSKUIDS"         => "badskuids/", // clean on full run
-    "MRSSIMPORT"        => "mrssimport/", // clean on full run
-    "POSTINGEST"        => "postingest/" // clean on full run
-
-/************* DONOT EDIT ABOVE ************************************************************
-** EOF GLOBALS
-************** DONOT EDIT ABOVE ************************************************************/
 
 $GLOBALS['fpi']                = array();
 $GLOBALS['fpi']['stamp']       = date("YdmHis");
